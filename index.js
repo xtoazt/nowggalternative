8f43adda6d8efe83bdf55ece2d346864d2a262e1
xtoazt <mynameisrohanandthisismyemail@gmail.com>
2025-08-10 12:27:57 -0400

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const PORT = 5000;
 app.use(cookieParser());
 
 const customProxy = createProxyMiddleware({
-  target: "https://arsenic.smartfoloo.space/",
+  target: "https://www.cloudmoonapp.com/",
   changeOrigin: true,
   onProxyReq: (proxyReq) => {
     storedCookies.forEach((cookie) => {
